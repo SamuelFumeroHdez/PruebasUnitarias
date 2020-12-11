dc8fad91d744e9e11594e647d97dba14a7e83a37
SamuelFumeroHdez <samuelfumerohdez@gmail.com>
2020-12-11 16:54:48 +0000

    Prueba de getNumberOfPassengers superada

--- src/test/java/es/ull/flights/FlightTest.java
+++ src/test/java/es/ull/flights/FlightTest.java
@@ -1,6 +1,7 @@
 package es.ull.flights;
 
 import es.ull.passengers.Passenger;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,15 +14,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class FlightTest {
 
     private Flight flight;
+    private Passenger passenger;
 
     @BeforeEach
     void setUp(){
         flight = new Flight("AB123",77);
+        passenger = new Passenger("ABC","Pepe", "ES");
+        flight.addPassenger(passenger);
     }
+
+
 
     @Test
     public void testGetFlightNumber(){
         assertEquals("AB123",flight.getFlightNumber());
+    }
+
+    @Test
+    public void testGetNumberOfPassengers(){
+        assertEquals(1,flight.getNumberOfPassengers());
     }
 
 
